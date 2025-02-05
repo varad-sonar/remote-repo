23ce58f9b424b1829570b84bf271881b820b39c1
varad-sonar <varadsonar2@gmail.com>
2025-02-05 10:55:53 +0530

    new syntax add

--- test.js
+++ test.js
@@ -1,19 +1,4 @@
-let someNumber = 33;
-let stringNumber = String(someNumber);
-//console.log(stringNumber);
-//console.log(typeof stringNumber);
+let score = 45
+console.log(typeof score);
+console.log(score);
 
-//******************************oparation***********************************
-
-let value = 45
-let negValue = -value
-console.log(negValue);
-
- console.log(50%100);
-console.log(20/2)
-
-console.log( (3 + 4) * 5 % 3);
-
-console.log(+true)
-
-consol.log(varad sonar);
