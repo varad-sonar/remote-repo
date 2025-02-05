828cf0ef7d17cf723f9c92ae82590cf09f37a6b0
varad-sonar <varadsonar2@gmail.com>
2025-02-05 10:40:53 +0530

    new js file add

---
+++ test.js
@@ -0,0 +1,17 @@
+let someNumber = 33;
+let stringNumber = String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//******************************oparation***********************************
+
+let value = 45
+let negValue = -value
+console.log(negValue);
+
+ console.log(50%100);
+console.log(20/2)
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true)
